3c832eed75b285f91a8ce54b0bfbbc5d4ef554bf
Deian Mosolov <deian.mosolov@gmail.com>
2017-05-25 15:15:28 -0400

    adds master layout basics

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="page-content">
+        <div class="container">
+            <!-- BEGIN PAGE CONTENT INNER -->
+            <div class="page-content-inner">
+                <div class="note note-info">
+                    <p> A black page template with a minimal dependency assets to use as a base for any
+                        custom page you create </p>
+                </div>
+            </div>
+            <!-- END PAGE CONTENT INNER -->
+        </div>
+    </div>
+@endsection
